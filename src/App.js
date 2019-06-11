5eb6d0e15fa33103a3dd661ba6d5a8cd5eadf7e5
Troy Budd <Troy.budd@cherrythinking.com>
2019-06-11 10:56:38 +0100

    TB - Added material ui, Added basic theme colors, Added basic Nav Bar and Buttons.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import { getWeatherData } from "./Services/fetchWeatherData";
 import InfoDisplay from "./Components/InfoDisplay";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+import { theme } from "./Ultils/theme";
+import { ThemeProvider } from "@material-ui/styles";
 
 const AppShell = styled.div`
   width: 100%;
@@ -26,9 +33,38 @@ class App extends React.Component {
 
   render() {
     return (
-      <AppShell>
-        <InfoDisplay weatherData={this.state.weatherData} />
-      </AppShell>
+      <>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <AppShell>
+            <AppBar className="top-bar" position="sticky" color="secondary">
+              <Grid className="top-bar__grid" container alignItems="center">
+                <Grid className="top-bar__grid-item" item xs={3}>
+                  <Button variant="contained" color="primary" size="medium">
+                    Warmest Capital{" "}
+                  </Button>
+                </Grid>
+                <Grid className="top-bar__grid-item" item xs={3}>
+                  <Button variant="contained" color="primary" size="medium">
+                    Coldest Capital
+                  </Button>
+                </Grid>
+                <Grid className="top-bar__grid-item" item xs={3}>
+                  <Button variant="contained" color="primary" size="medium">
+                    Warmest Country
+                  </Button>
+                </Grid>
+                <Grid className="top-bar__grid-item" item xs={3}>
+                  <Button variant="contained" color="primary" size="medium">
+                    Coldest Country
+                  </Button>
+                </Grid>
+              </Grid>
+            </AppBar>
+            <InfoDisplay weatherData={this.state.weatherData} />
+          </AppShell>
+        </ThemeProvider>
+      </>
     );
   }
 }
